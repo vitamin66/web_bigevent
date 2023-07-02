bb4e12159437eb27941d950bc1bdb30c9acb2291
vitamin66 <136416208+vitamin66@users.noreply.github.com>
2023-07-02 19:46:12 +0800

    Update baseAPI.js

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,16 @@
+// 调用 ajaxPrefilter方法 在请求发起ajax 请求是 会经过改函数
+// 可以在改函数里写 headers 请求头 和 url 地址的拼接
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://127.0.0.1' + options.url
+
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+})
+
 
 
 
